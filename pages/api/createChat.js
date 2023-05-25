1b15c722d9bc28b667f4b7e569d9d30ed5a3ff49
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-05-25 12:10:11 -0600

    Update

--- pages/api/createChat.js
+++ pages/api/createChat.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   const prisma = new PrismaClient();
   const chat = await prisma.chat.create({
     data: {
-      adventure_id: { connect: { id: aId }},
+      adventure:  aId ,
       match_id: { connect: { id: mId }},
       active: true
     },
